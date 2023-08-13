17996e7ed42f2b6b32c3e68741e17712b1565803
AdvenAdam <advenadam@student.uns.ac.id>
2023-08-13 19:59:29 +0700

    rest api with sanctum

---
+++ app/Filters/ApiFilter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filters;
+
+use Illuminate\Http\Request;
+
+class ApiFilter
+{
+    protected $safeParms = [];
+
+    protected $colmnMap = [];
+
+    protected $operatorMap = [];
+
+    public function transform(Request $request)
+    {
+        $eloQuery = [];
+
+        foreach ($this->safeParms as $parm => $operators) {
+            $query = $request->query($parm);
+
+            if (!isset($query)) {
+                continue;
+            }
+            $column = $parm;
+
+            foreach ($operators as $operator) {
+                if (isset($query[$operator])) {
+                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+                }
+            }
+        }
+
+        return $eloQuery;
+    }
+}
